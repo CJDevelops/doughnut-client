e2cd36a5d52c39e4c593f90f71ff874d2576dcac
cjdevelops <cjdevelops@gmail.com>
2024-04-09 07:48:18 +0000

    Apr 9, 2024, 9:48 AM

--- src/Components/Transaction/TransactionList.jsx
+++ src/Components/Transaction/TransactionList.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Transaction from './Transaction';
+import axios from 'axios';
 
 function TransactionList() {
+    const [transactions, setTransactions] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/transactions')
+            .then(response => {
+                setTransactions(response.data);
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }, []);
+
     return (
         <div>
             <h6>Transaction List</h6>
-            <Transaction />
+            {transactions.map(transaction => (
+                <Transaction key={transaction.id} transaction={transaction} />
+            ))}
         </div>
     );
 }
