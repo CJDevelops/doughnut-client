e2d228d0c33d70c59260fa27a936ff49b6eaae21
cjdevelops <cjdevelops@gmail.com>
2024-04-09 08:24:10 +0000

    Apr 9, 2024, 10:24 AM

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 
 function TopBar() {
     return (
         <Navbar className="bg-body-tertiary">
+            <Container>
             <Navbar.Brand href="#home">Doughnut</Navbar.Brand>
             <Nav className="me-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
                 <Nav.Link href="#features">Features</Nav.Link>
                 <Nav.Link href="#pricing">Pricing</Nav.Link>
             </Nav>
+                </Container>
         </Navbar>
     );
 }
