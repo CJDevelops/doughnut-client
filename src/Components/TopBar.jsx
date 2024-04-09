f3c92a9491deb5654d393c026f17c43406e35fcf
cjdevelops <cjdevelops@gmail.com>
2024-04-09 08:24:41 +0000

    Apr 9, 2024, 10:24 AM

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -7,13 +7,13 @@ function TopBar() {
     return (
         <Navbar className="bg-body-tertiary">
             <Container fluid>
-            <Navbar.Brand href="#home">Doughnut</Navbar.Brand>
-            <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-                </Container>
+                <Navbar.Brand>Doughnut</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link>Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+            </Container>
         </Navbar>
     );
 }
