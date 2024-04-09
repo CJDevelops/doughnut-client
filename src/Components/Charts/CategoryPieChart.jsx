8fc29f7e22d0d5aa7100b53462708b4313b2597e
cjdevelops <cjdevelops@gmail.com>
2024-04-09 07:34:45 +0000

    Apr 9, 2024, 9:34 AM

---
+++ src/Components/Charts/CategoryPieChart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CategoryPieChart() {
+    return (
+        <div>
+            <h1>Transaction List</h1>
+        </div>
+    );
+}
+
+export default CategoryPieChart;
