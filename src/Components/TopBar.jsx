bfbb14d95d206639c0313e1a106f322f1c1efaa9
cjdevelops <cjdevelops@gmail.com>
2024-04-09 08:22:19 +0000

    Apr 9, 2024, 10:22 AM

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import Nav from 'react-bootstrap/Nav';
 
 function TopBar() {
     return (
-        <div>
-            <h1>Donut</h1>
-        </div>
+        <Nav>
+            <Nav.Item>
+                <Nav.Link href="/transactions">Transactions</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href="/categories">Categories</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href="/subcategories">Subcategories</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href="/currencies">Currencies</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href="/users">Users</Nav.Link>
+            </Nav.Item>
+        </Nav>
     );
 }
 
