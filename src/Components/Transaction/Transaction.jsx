954aadbb98af7de60e7f74e8015e7625bb7df779
cjdevelops <cjdevelops@gmail.com>
2024-04-09 07:52:47 +0000

    Apr 9, 2024, 9:52 AM

--- src/Components/Transaction/Transaction.jsx
+++ src/Components/Transaction/Transaction.jsx
@@ -4,16 +4,16 @@ import PropTypes from 'prop-types';
 function Transaction({ transaction }) {
     return (
         <>
-            <td>Transaction ID: {transaction.transaction_id}</td>
-            <td>Name: {transaction.transaction_name}</td>
-            <td>Date: {new Date(transaction.transaction_date).toLocaleDateString()}</td>
-            <td>Amount: {transaction.transaction_amount}</td>
-            <td>Category ID: {transaction.category_id}</td>
-            <td>Subcategory ID: {transaction.subcategory_id}</td>
-            <td>Currency ID: {transaction.currency_id}</td>
-            <td>User ID: {transaction.user_id}</td>
-            <td>Type: {transaction.transaction_type}</td>
-            </>
+            <td>{transaction.transaction_id}</td>
+            <td>{transaction.transaction_name}</td>
+            <td>{new Date(transaction.transaction_date).toLocaleDateString()}</td>
+            <td>{transaction.transaction_amount}</td>
+            <td>{transaction.category_id}</td>
+            <td>{transaction.subcategory_id}</td>
+            <td>{transaction.currency_id}</td>
+            <td>{transaction.user_id}</td>
+            <td>{transaction.transaction_type}</td>
+        </>
     );
 }
 
