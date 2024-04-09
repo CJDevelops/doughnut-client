c4c4ea0b6f64e0fc173baa831bb8ea7f90d16c20
cjdevelops <cjdevelops@gmail.com>
2024-04-09 08:49:55 +0000

    Apr 9, 2024, 10:49 AM

--- src/Components/Transaction/AddTransaction.jsx
+++ src/Components/Transaction/AddTransaction.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import Button from 'react-bootstrap/Button';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
 const AddTransaction = () => {
     return (
         <div>
