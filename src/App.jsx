7f10b81bf667f66637c4717cffbb68aba0a9cae1
cjdevelops <cjdevelops@gmail.com>
2024-04-09 07:34:15 +0000

    Apr 9, 2024, 9:34 AM

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import './App.css'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Table from 'react-bootstrap/Table'
 
 import TransactionList from './Components/Transaction/TransactionList'
 
