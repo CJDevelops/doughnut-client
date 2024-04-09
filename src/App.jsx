8673f83261d935a52293e7acbddd3b79fb2a22b7
cjdevelops <cjdevelops@gmail.com>
2024-04-09 07:29:32 +0000

    Apr 9, 2024, 9:29 AM

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@ import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+
 function App() {
   
 
   return (
     <>
-      <h1>Donut</h1>
+      <Container fluid>
+        <Row>
+          <Col>
+            <h1>Donut</h1>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
