7b7193ca3a7edfaaf2e7064dde09f65e7370bca6
cjdevelops <cjdevelops@gmail.com>
2024-04-09 08:52:04 +0000

    Apr 9, 2024, 10:52 AM

--- src/Components/Transaction/AddTransaction.jsx
+++ src/Components/Transaction/AddTransaction.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import { useState } from 'react';
 
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const AddTransaction = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <>
-            <Button variant="primary"><FontAwesomeIcon icon={faPlus} /></Button>
+            <Button variant="primary" onClick={handleShow}><FontAwesomeIcon icon={faPlus} /></Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Transaction</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Modal body text goes here.</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </>        
     );
 };
